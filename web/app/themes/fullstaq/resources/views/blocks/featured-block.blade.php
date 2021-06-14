784d0a2335d80a67e35f4b08db1ceb2f272b1dbe
SB-CHAUHO\ChauHo <chau.ho@sunbytes.vn>
2021-06-14 18:13:33 +0700

    Build block featured

--- web/app/themes/fullstaq/resources/views/blocks/featured-block.blade.php
+++ web/app/themes/fullstaq/resources/views/blocks/featured-block.blade.php
@@ -14,17 +14,30 @@
   $block_data = \App\Controllers\App::getFeaturedBlockData();
 @endphp
 @if(!empty($block_data))
-  <div {!! $block_data['block_id'] !!}>
+  <div {!! $block_data['block_id'] !!} class="featured-block">
     @unless(empty($block_data['block_title']))
-      {!! $block_data['block_title'] !!}
+      <h2 class="featured-block__title text-center">{!! $block_data['block_title'] !!}</h2>
     @endunless
-      @include('partials.overview.overview-item', ['article' => $block_data['featured_article_1']])
-      @include('partials.overview.overview-item', ['article' => $block_data['featured_article_2']])
-      @include('partials.overview.overview-item', ['article' => $block_data['featured_article_3']])
-
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-4 featured-block__item {{ $block_data['layout_style'] === 'large' ? 'col-lg-6 featured-block__item--large' : '' }}">
+          @include('partials.overview.overview-item', ['article' => $block_data['featured_article_1']])
+        </div>
+        <div class="col-12 col-md-4 featured-block__item {{ $block_data['layout_style'] === 'large' ? 'col-lg-3' : '' }}">
+          @include('partials.overview.overview-item', ['article' => $block_data['featured_article_2']])
+        </div>
+        <div class="col-12 col-md-4 featured-block__item {{ $block_data['layout_style'] === 'large' ? 'col-lg-3' : '' }}">
+          <div class="{{ $block_data['layout_style'] === 'large' ? 'featured-block__item--purple' : '' }}">
+            @include('partials.overview.overview-item', ['article' => $block_data['featured_article_3']])
+          </div>
+        </div>
+      </div>
       @unless(empty($block_data['link']['title']) || empty($block_data['link']['url']))
-        {!! $block_data['link']['title'] !!}
+        <div class="text-center">
+          <a class="btn-readmore" href="{{ $block_data['link']['url'] }}">{!! $block_data['link']['title'] !!}</a>
+        </div>
       @endunless
+    </div>
   </div>
 @elseif(is_admin())
   <h3>Featured Block</h3>
--- web/app/themes/fullstaq/resources/views/partials/entry-meta.blade.php
+++ web/app/themes/fullstaq/resources/views/partials/entry-meta.blade.php
@@ -1,9 +1,22 @@
 @php
   $post_id = !empty($id) ? $id : get_the_ID();
-  $category = App\get_primary_category($post_id)
+  $category = App\get_primary_category($post_id);
+  $is_cases_type = get_post_type($post_id) === 'cases' ;
 @endphp
-{{ get_the_date(get_option('date_format'), $post_id) }}
-@if(!empty($category->name))
-  {!! $category->name !!}
-@endif
-
+<div class="time-cate">
+  @if (is_singular('post'))
+    <span class="time-cate__author">{{ sprintf(__('by %s', 'fullstaq'), get_the_author()) }}</span>
+    <span class="time-cate__line">|</span>
+  @endif
+  @if (!$is_cases_type)
+    <span class="time-cate__time text-uppercase">
+      {{ get_the_date(get_option('date_format'), $post_id) }}
+    </span>
+  @endif
+  @if(!empty($category->name))
+    @if (!$is_cases_type)
+      <span class="time-cate__line">|</span>
+    @endif
+    <span class="time-cate__category">{!! $category->name !!}</span>
+  @endif
+</div>
