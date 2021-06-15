36844831d5ab15870b0a89561e5733e498458614
SB-CHAUHO\ChauHo <chau.ho@sunbytes.vn>
2021-06-15 15:49:05 +0700

    Build header

--- web/app/themes/fullstaq/resources/assets/scripts/routes/common.js
+++ web/app/themes/fullstaq/resources/assets/scripts/routes/common.js
@@ -16,9 +16,31 @@ const slider = () => {
   });
 };
 
+const scroll = () => {
+  $(window).scroll(function () {
+    $(window).scrollTop() > 5 ? $('.js-header').addClass('header--scroll') : $('.js-header').removeClass('header--scroll');
+  });
+};
+
+const menuEvent = function () {
+  $('.header .dropdown > a').on('click', function (event) {
+    event.preventDefault();
+
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active').next().slideUp();
+    } else {
+      $('.header .dropdown > a.active').removeClass('active').next().slideUp();
+
+      $(this).addClass('active').next().slideDown();
+    }
+  });
+}
+
 export default {
   init() {
     slider();
+    scroll();
+    menuEvent();
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
