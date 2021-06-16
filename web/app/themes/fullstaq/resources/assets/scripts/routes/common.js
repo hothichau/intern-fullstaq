6913d9da093dec7359360ca205f7a7683728fe64
SB-CHAUHO\ChauHo <chau.ho@sunbytes.vn>
2021-06-16 10:00:41 +0700

    Fixed files

--- web/app/themes/fullstaq/resources/assets/scripts/routes/common.js
+++ web/app/themes/fullstaq/resources/assets/scripts/routes/common.js
@@ -1,18 +1,30 @@
 const slider = () => {
   $('.js-icon-slider').slick({
-    slidesToShow: 2,
-      slidesToScroll: 2,
-      dots: true,
-      mobileFirst: true,
-      responsive: [
-        {
-          breakpoint: 540,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 4,
-          },
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
         },
-      ],
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+    ],
   });
 };
 
