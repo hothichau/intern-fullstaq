6752eb6c4bf0ce859ef3348ff4db1419d4d8d117
SB-CHAUHO\ChauHo <chau.ho@sunbytes.vn>
2021-06-18 10:02:27 +0700

    Fixed files

--- web/app/themes/fullstaq/resources/views/partials/single/cases-service-block.blade.php
+++ web/app/themes/fullstaq/resources/views/partials/single/cases-service-block.blade.php
@@ -5,19 +5,19 @@
     <div class="container">
       <div class="row">
         @unless(empty($service_block['column_1']['title']) || empty(Arr::get($service_block, 'column_1.icon.ico', '')) || empty($service_block['column_1']['txt']))
-        <div class="col-lg-4 service-block__item">
-          @include($partial_slug, $service_block['column_1'])
-        </div>
+          <div class="col-12 col-md-3 col-lg-4 service-block__item">
+            @include($partial_slug, $service_block['column_1'])
+          </div>
         @endunless
         @unless(empty($service_block['column_2']['title']) || empty(Arr::get($service_block, 'column_2.icon.ico', '')) || empty($service_block['column_2']['txt']))
-        <div class="col-lg-4 service-block__item">
-          @include($partial_slug, $service_block['column_2'])
-        </div>
+          <div class="col-12 col-md-3 col-lg-4 service-block__item">
+            @include($partial_slug, $service_block['column_2'])
+          </div>
         @endunless
         @unless(empty($service_block['column_3']['title']) || empty(Arr::get($service_block, 'column_3.icon.ico', '')) || empty($service_block['column_3']['links']))
-        <div class="col-lg-4 service-block__item">
-          @include($partial_slug, $service_block['column_3'])
-        </div>
+          <div class="col-12 col-md-3 col-lg-4 service-block__item">
+            @include($partial_slug, $service_block['column_3'])
+          </div>
         @endunless
       </div>
     </div>
--- web/app/themes/fullstaq/resources/views/partials/single/single-share-buttons.blade.php
+++ web/app/themes/fullstaq/resources/views/partials/single/single-share-buttons.blade.php
@@ -1,7 +1,7 @@
 <div class="social-share d-flex justify-content-center align-items-center">
-  <span class="social-share__label">Share this</span>
-  <a class="social-share__link" href="https://twitter.com/intent/tweet?text={{ get_the_permalink() }}" target="_blank"><i class="fab fa-twitter"></i></a>
-  <a class="social-share__link" href="https://www.facebook.com/sharer/sharer.php?u={{ get_the_permalink() }}" target="_blank"><i class="fab fa-facebook-f"></i></a>
-  <a class="social-share__link" href="https://www.linkedin.com/shareArticle?mini=true&url={{ get_the_permalink() }}" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-  <a class="social-share__link" href="mailto:?&subject=&body={{ get_the_permalink() }}" target="_blank"><i class="fas fa-envelope"></i></a>
+  <span class="social-share__label text-uppercase">Share this</span>
+  <a class="social-share__link text-center" href="https://twitter.com/intent/tweet?text={{ get_the_permalink() }}" target="_blank"><i class="fab fa-twitter"></i></a>
+  <a class="social-share__link text-center" href="https://www.facebook.com/sharer/sharer.php?u={{ get_the_permalink() }}" target="_blank"><i class="fab fa-facebook-f"></i></a>
+  <a class="social-share__link text-center" href="https://www.linkedin.com/shareArticle?mini=true&url={{ get_the_permalink() }}" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+  <a class="social-share__link text-center" href="mailto:?&subject=&body={{ get_the_permalink() }}" target="_blank"><i class="fas fa-envelope"></i></a>
 </div>
