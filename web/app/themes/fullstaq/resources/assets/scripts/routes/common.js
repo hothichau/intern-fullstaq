5ad232c79c77a7450b2f650ca694c253ca656c70
LAPTOP-971I83D3\chauh <chau.ho@sunbytes.vn>
2021-06-24 16:37:38 +0700

    Update file

--- web/app/themes/fullstaq/resources/assets/scripts/routes/common.js
+++ web/app/themes/fullstaq/resources/assets/scripts/routes/common.js
@@ -76,6 +76,8 @@ export default {
     slider();
     scroll();
     menuEvent();
+    initSelect();
+    customUpload();
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
